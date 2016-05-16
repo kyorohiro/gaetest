90b56cf7a7923585bf363bcc22d1e360b2366e41
kyorohiro <kyorohiro@gmail.com>
2016-05-16 15:41:50 +0900

    ..

--- dart/bin/main.dart
+++ dart/bin/main.dart
@@ -1,9 +1,68 @@
 import "package:test/test.dart" as test;
 
+import "package:tetorica/http.dart" as http;
+import "package:tetorica/net_dartio.dart" as netio;
+import "dart:convert" as conv;
+
 main(List<String> args) {
-  print('Hello world!');
+  print('Hello world! ${args}' );
+  String host = args[0];
+  int port = int.parse(args[1]);
+  int i=0;
   test.group("", (){
-    test.test("test", (){
+    test.test("test1", () async {
+      var builder = new netio.TetSocketBuilderDartIO();
+      var client = new http.HttpClient(builder);
+      await client.connect(host, port);
+      http.HttpClientResponse response = await client.post("/user/new", conv.UTF8.encode(conv.JSON.encode({
+        "name":"kyoro001",//
+        "mail":"kyoro001@example.com",//
+        "pass":"asdfasdf",//
+      })));
+      print("## ${i++} ## ${await response.body.getString()}");
+      test.expect(true, true);
+    });
+    test.test("test2a", () async {
+      var builder = new netio.TetSocketBuilderDartIO();
+      var client = new http.HttpClient(builder);
+      await client.connect(host, port);
+      http.HttpClientResponse response = await client.post("/user/get", conv.UTF8.encode(conv.JSON.encode({
+        "name":"kyoro001",//
+      })));
+      print("## ${i++} ## ${await response.body.getString()}");
+      test.expect(true, true);
+    });
+    test.test("test2b", () async {
+      var builder = new netio.TetSocketBuilderDartIO();
+      var client = new http.HttpClient(builder);
+      await client.connect(host, port);
+      http.HttpClientResponse response = await client.post("/user/get", conv.UTF8.encode(conv.JSON.encode({
+        "name":"kyoro002",//
+      })));
+      print("## ${i++} ## ${await response.body.getString()}");
+      test.expect(true, true);
+    });
+    //
+    test.test("test3", () async {
+      var builder = new netio.TetSocketBuilderDartIO();
+      var client = new http.HttpClient(builder);
+      await client.connect(host, port);
+      http.HttpClientResponse response = await client.post("/user/mail/getUser", conv.UTF8.encode(conv.JSON.encode({
+        "mail":"kyoro001@example.com",//
+      })));
+      print("## ${i++} ## ${await response.body.getString()}");
+      test.expect(true, true);
+    });
+
+    test.test("test4", () async {
+      var builder = new netio.TetSocketBuilderDartIO();
+      var client = new http.HttpClient(builder);
+      await client.connect(host, port);
+      http.HttpClientResponse response = await client.post("/user/updateMail", conv.UTF8.encode(conv.JSON.encode({
+        "name":"kyoro001",//
+        "mail":"kyoro001b@example.com",//
+      })));
+      print("## ${i++} ## ${await response.body.getString()}");
       test.expect(true, true);
     });
   });
--- go/app.go
+++ go/app.go
@@ -1,6 +1,7 @@
 package hello
 
 import (
+	"encoding/json"
 	"fmt"
 	"gaeuser"
 	"net/http"
@@ -16,16 +17,85 @@ func init() {
 
 	http.HandleFunc("/user/new", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello World!!")
+		ctx := appengine.NewContext(r)
 		data := GetParam(r)
-		user := gaeuser.NewUser(appengine.NewContext(r), data["name"].(string))
-		err := user.PullFromDB(ctx)
-		err = user.Regist(ctx, data["pass"].(string), data["mail"].(string))
+		user := gaeuser.NewUser(ctx, data["name"].(string))
+		err := user.Regist(ctx, data["pass"].(string), data["mail"].(string))
+		if err != nil {
+			Response(w, map[string]interface{}{"r": "ng", "s": err.Error()})
+			return
+		}
 		//
-		Response(map[string]string{"r": "ok", "s": "good"})
+		Response(w, map[string]interface{}{"r": "ok", "s": "good"})
+	})
+
+	http.HandleFunc("/user/updateMail", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World!!")
+		ctx := appengine.NewContext(r)
+		data := GetParam(r)
+		user := gaeuser.NewUser(ctx, data["name"].(string))
+		err := user.UpdateMail(ctx, data["mail"].(string))
+		if err != nil {
+			Response(w, map[string]interface{}{"r": "ng", "s": err.Error()})
+			return
+		}
+		//
+		Response(w, map[string]interface{}{"r": "ok", "s": "good"})
+	})
+
+	http.HandleFunc("/user/get", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World!!")
+		ctx := appengine.NewContext(r)
+		data := GetParam(r)
+		user := gaeuser.NewUser(ctx, data["name"].(string))
+		err := user.PullFromDB(ctx)
+		if err != nil {
+			Response(w, map[string]interface{}{"r": "ng", "s": err.Error()})
+			return
+		}
+		//
+		Response(w, map[string]interface{}{
+			"r": "ok", "s": "good", //
+			"name":     user.GaeObject.UserName, //
+			"created":  user.GaeObject.Created,  //
+			"logined":  user.GaeObject.Logined,  //
+			"loginid":  user.GaeObject.LoginId,  //
+			"mail":     user.GaeObject.Mail,     //
+			"passHash": user.GaeObject.PassHash, //
+			"meicon":   user.GaeObject.MeIcon,   //
+		})
+	})
+
+	http.HandleFunc("/user/mail/getUser", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World!!")
+		ctx := appengine.NewContext(r)
+		data := GetParam(r)
+
+		mail := gaeuser.NewMail(ctx, data["mail"].(string))
+		err := mail.PullFromDB(ctx)
+		if err != nil {
+			Response(w, map[string]interface{}{"r": "ng", "s": err.Error()})
+			return
+		}
+		user, err1 := mail.GetUser(ctx)
+		if err1 != nil {
+			Response(w, map[string]interface{}{"r": "ng", "s": err1.Error()})
+			return
+		}
+
+		//
+		Response(w, map[string]interface{}{"r": "ok", "s": "good",
+			"mail_mail":    mail.GaeObject.Mail,     //
+			"mail_name":    mail.GaeObject.UserName, //
+			"user_name":    user.GaeObject.UserName,
+			"user_created": user.GaeObject.Created,
+			"user_logined": user.GaeObject.Logined,
+			"user_loginid": user.GaeObject.LoginId,
+		})
 	})
 }
 
-func Response(v map[string]interface{}) {
+func Response(w http.ResponseWriter, v map[string]interface{}) {
 	b, _ := json.Marshal(v)
 	fmt.Fprintln(w, string(b))
 }
